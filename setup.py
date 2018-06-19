ffda9501794657377f34c6dc51145beb1086e959
Tony Roberts <tony@pyxll.com>
2018-06-19 14:36:12 +0100

    Update setup.py to version 2.1

--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@ from setuptools import setup, find_packages
 setup(name='pyxll_utils',
       version='2.0',
       author='PyXLL Ltd, Enthought Inc.',
+      license='BSD 2-Clause "Simplified" License',
+      long_description=open('README.md').read(),
       packages=find_packages(),
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
+      setup_requires=[
+          'pypiwin32',
+          'stevedore',
+          'pytest-runner',
+      ],
+      tests_require=[
+          'pytest'
+      ],
       # Provides a namespace for extension points to contribute to. This
       # functionality is required by the pyxll_addons.extension_loader module
       provides=['pyxll.extensions'])
